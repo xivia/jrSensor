d28a9eea2648ab42b7e89c781e984e52b061f508
jherzig <info@jherzig.ch>
2015-04-28 21:41:37 +0200

    Überarbeitet und die Methode getTemp hinzugefügt jetzt wird mit dem POST die ausgelesende Temperatur und Luftfeuchtikeit als String in die DB gespeichert.

---
+++ jrSensorArduino/sketch_RestFul_GET_POST/sketch_RestFul_GET_POST.ino
@@ -0,0 +1,188 @@
+/*
+  Web client
+  20150419 J.Herzig 
+
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13
+
+
+ */
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <dht.h>
+
+#define DHT11_PIN 2
+dht DHT;
+
+// Bei mehreren Arduinos muss die MAC unterschiedlich sein
+byte MAC[] = {0x90, 0xA2, 0xDA, 0x0D, 0x4E, 0x71 };
+byte SERVER[] = { 192, 168, 79, 10 }; // WebServer Rasspary Pi
+int PORT = 8080;  // Port zu WildFly
+EthernetClient client;
+boolean DEBUGGER = false;
+boolean clientConnected = false;
+double temperatur;
+
+void setup() {
+  Serial.begin(9600);
+  delay(5000);
+}
+
+void loop()
+{
+  // Wichtig bei starten des Boards funktioniert die Verbindung erst nach mehreren Versuchen.
+  while(!clientConnected)
+  {
+    connectServer();
+  }
+  if(clientConnected)
+  {
+    testServicePOST();
+    checkStatus();
+  }
+  
+  
+}
+
+void connectServer(){
+  // Ethernet verbindung mit DHCP aufbauen
+  if (Ethernet.begin(MAC) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+  }
+  if (DEBUGGER){
+    Serial.println("connecting...");
+    Serial.print("Obtaining local IP: --> ");
+    Serial.println(Ethernet.localIP());
+  }
+  
+  // Verbindung mit dem Server herstellen
+  delay(500);
+  if (client.connect(SERVER, PORT)) {
+    if (DEBUGGER){
+      Serial.println("connected");
+    }
+    clientConnected = true;
+  } 
+  else {
+    Serial.println("connection failed");
+  }
+}
+
+void testService()
+{
+   client.println("GET /jrLinkWeb/ws/test HTTP/1.0");
+   client.println();
+   if (DEBUGGER){
+     // Warten da sonst der Client nicht verfügbar ist
+     delay(1000);
+     Serial.print("nach dem GET --> client.available() --> ");
+     Serial.println(client.available());
+   }  
+}
+
+void testServiceGET()
+{
+   client.println("GET /jrLinkWeb/ws/link/?u=admin&p=123 HTTP/1.0");
+   client.println();
+   
+   if (DEBUGGER){
+     // Warten da sonst der Client nicht verfügbar ist
+     delay(1000);
+     Serial.print("nach dem GET --> client.available() --> ");
+     Serial.println(client.available());
+   }  
+}
+
+void testServicePOST()
+{  
+  String temperatur;
+  getTemp(temperatur);
+  String tempData = temperatur + "\"}";
+
+  
+  String data = "{ \"name\" : \"testJHE\", \"url\" : \"" + tempData;
+  
+  if (DEBUGGER){
+    Serial.print("data POST String --> ");
+    Serial.println(data);
+  }
+
+  client.println("POST /jrLinkWeb/ws/link/?u=admin&p=123 HTTP/1.1"); 
+  client.println("Content-Type: application/json"); 
+  client.print("Content-Length: "); 
+  client.println(data.length()); 
+  client.println(); 
+  client.print(data);
+  client.println();
+  
+  if (DEBUGGER){
+    // Delay wird für die Ausgabe über Serial.print benötigt
+    delay(1000);
+    Serial.print("nach dem POST --> client.available() --> ");
+    Serial.println(client.available());
+  }  
+
+}
+
+void checkStatus()
+{
+  if (clientConnected)
+  {    
+    // Ausgabe der Daten
+    while (client.available())
+    {
+      char c = client.read();
+      Serial.print(c);
+    }
+
+    client.stop();
+    
+    if (!client.connected()) 
+    {
+      if (DEBUGGER){
+        Serial.println("disconnecting.");
+        Serial.println();
+      }
+    clientConnected = false;
+    }
+  }
+}
+
+void getTemp(String &dest)
+{
+   // READ DATA
+  int chk = DHT.read33(DHT11_PIN);
+  switch (chk)
+  {
+    case DHTLIB_OK:  
+		//Serial.print("OK,\t"); 
+		break;
+    case DHTLIB_ERROR_CHECKSUM: 
+		Serial.print("Checksum error,\t"); 
+		break;
+    case DHTLIB_ERROR_TIMEOUT: 
+		Serial.print("Time out error,\t"); 
+		break;
+    case DHTLIB_ERROR_CONNECT:
+        Serial.print("Connect error,\t");
+        break;
+    case DHTLIB_ERROR_ACK_L:
+        Serial.print("Ack Low error,\t");
+        break;
+    case DHTLIB_ERROR_ACK_H:
+        Serial.print("Ack High error,\t");
+        break;
+    default: 
+		Serial.print("Unknown error,\t"); 
+		break;
+  }
+  // DISPLAY DATA
+  String data;
+  //data = String(DHT.humidity, 1);
+  data = String(DHT.humidity, 1) + ",Temp: ";
+  data = data + String(DHT.temperature, 1);
+  dest = data;
+}
+
+
